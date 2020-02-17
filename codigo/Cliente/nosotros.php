477092f4a6aa439a5f0794652ed7e6adc41af3be
Cristofer Chamorro <crischaalpizar@gmail.com>
2020-02-17 14:11:37 -0600

    Estructura del proyecto

---
+++ codigo/Cliente/nosotros.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <?php
+        require_once("../public/head.php");
+    ?>
+    <title>Nosotros</title>
+</head>
+<body>
+
+<div class="wrapper home2">
+    <!--Header Start-->
+    <header class="header-style-2">
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="index.html"><img src="images/h2logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item ">
+                        <a class="nav-link " href="index.html"  > INICIO </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="Productos.html"  > PRODUCTOS </a>
+
+                    </li>
+
+                    <li class="nav-item ">
+                        <a class="nav-link " href="Servicios.html"  > SERVICIOS </a>
+
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="Noticia.html"  > NOTICIAS </a>
+
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="Proyectos.html"  > PROYECTOS </a>
+
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="Galeria.html" > GALERIA </a>
+
+                    </li>
+
+                    <li class="nav-item ">
+                        <a class="nav-link " href="#"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> BIBLIOTECA </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!--Header End-->
+    <!--Slider Start-->
+    <section id="home-slider" class="owl-carousel owl-theme wf100">
+        <div class="item">
+            <div class="slider-caption h2slider">
+                <div class="container">
+
+                </div>
+            </div>
+            <img src="../images/h2-slide1.jpg" alt="">
+        </div>
+        <div class="item">
+            <div class="slider-caption h2slider">
+                <div class="container">
+
+                </div>
+            </div>
+            <img src="../images/h2-slide2.jpg" alt="">
+        </div>
+        <div class="item">
+            <div class="slider-caption h2slider">
+                <div class="container">
+
+                </div>
+            </div>
+            <img src="../images/h2-slide3.jpg" alt="">
+        </div>
+    </section>
+
+    <div class="about-video-section wf100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="about-text">
+                        <h5>We are Taking Small Steps to Make</h5>
+                        <h2>Earth Better Planet</h2>
+                        <p><strong>One of the serious issues faced by our modern world is environmental pollution, and it is one the most significant challenges that the world is facing in our day.</strong></p>
+                        <p>Environmental pollution has existed for centuries. But, it began to be a severe issue following the industrial revolution in the 19th century. Pollution generally takes place when pollutants pollute the natural surroundings and it can bring about changes that affect our everyday lifestyles unfavorably. </p>
+
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about-video-img"> <img src="../images/aboutimg.jpg" alt=""> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Footer Start-->
+    <footer class="footer">
+        <div class="footer-top wf100">
+            <div class="container">
+                <div class="row center">
+                    <div class="col-lg-2 col-md-6 col-sm-6"></div>
+                    <div class="col-lg-5 col-md-6 col-sm-6">
+                        <!--Footer Widget Start-->
+                        <div class="footer-widget">
+                            <h4>Contactos</h4>
+                            <p> Telefonos: </p>
+                            <!--  <ul >
+                                 <li><a href="#">8888 8888</a></li>
+                                 <li><a href="#">5555 5555</a></li>
+                              </ul>-->
+                            <p> Correos electronicos: </p>
+                            <!--   <ul >
+                                  <li><a href="#">asirea@gmail.com</a></li>
+                               </ul>-->
+                        </div>
+                        <!--Footer Widget End-->
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-sm-6">
+                        <!--Footer Widget Start-->
+                        <div class="footer-widget">
+                            <h4>Redes</h4>
+                            <!--    <ul class="quick-links">
+                                 <li><a href="#">Facebook</a></li>
+
+                              </ul>-->
+                        </div>
+                        <!--Footer Widget End-->
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+    </footer>
+    <!--Footer End-->
+</div>
+
+</body>
+</html>
