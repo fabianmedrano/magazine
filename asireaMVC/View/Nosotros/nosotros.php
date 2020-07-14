08040c00662555663ec39e3133055437b2937861
Fabian <edoardfabian@gmail.com>
2020-07-13 18:33:49 -0600

    carusel
    30 minutos

--- asireaMVC/View/Nosotros/nosotros.php
+++ asireaMVC/View/Nosotros/nosotros.php
@@ -16,9 +16,8 @@ require_once CONTROLLER_PATH . "/nosotros/nosotros_controller.php";
   <link href="../../public/css/general.css" rel="stylesheet">
 -->
 
-  
-<link rel="stylesheet" href="<?php echo LIB_PATH ?>/fontawesome/css/fontawesome.min.css">
-    <link href="<?php echo LIB_PATH ?>/fontawesome/css/all.min.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="<?php echo LIB_PATH ?>/fontawesome/css/fontawesome.min.css">
   <link href="<?php echo PUBLIC_PATH ?>/css/nosotros/nosotros.css" rel="stylesheet">
   <!-- CSS FILES START-->
   <link href="<?php echo LIB_PATH ?>/template/css/custom.css" rel="stylesheet">
@@ -32,15 +31,15 @@ require_once CONTROLLER_PATH . "/nosotros/nosotros_controller.php";
 
   <!--   JS Files Start  -->
   <script src="<?php echo LIB_PATH ?>/jquery/jquery-3.2.1.min.js"></script>
-  <script src="<?php echo LIB_PATH ?>/jquery/jquery-migrate-1.4.1.min.js"></script> 
+  <script src="<?php echo LIB_PATH ?>/jquery/jquery-migrate-1.4.1.min.js"></script>
   <script src="<?php echo LIB_PATH ?>/bootstrap/js/bootstrap.min.js"></script>
   <script src="<?php echo LIB_PATH ?>/template/js/owl.carousel.min.js"></script>
   <script src="<?php echo LIB_PATH ?>/jquery/jquery.prettyPhoto.js"></script>
   <script src="<?php echo LIB_PATH ?>/template/js/custom.js"></script>
-  <script src="<?php echo LIB_PATH ?>/template/js/popper.min.js"></script> 
+  <script src="<?php echo LIB_PATH ?>/template/js/popper.min.js"></script>
   <script src="<?php echo LIB_PATH ?>/template/js/isotope.min.js"></script>
   <!--   JS Files END  -->
-  
+
 
   <?php include(TEMPLATES_PATH . "/metadata.php") ?>
   <title>Nosotros</title>
@@ -48,73 +47,76 @@ require_once CONTROLLER_PATH . "/nosotros/nosotros_controller.php";
 </head>
 
 <body>
-  <?php include(TEMPLATES_PATH . "/header.php") 
+  <?php include(TEMPLATES_PATH . "/header.php")
   ?>
+
 
   <div class="container_principal">
 
+
+
+
+
     <!-- INICIO CARRUSEL -->
 
-    <section id="home-slider" class=" carusel owl-carousel owl-theme wf100 owl-loaded owl-drag">
-      <div class="owl-stage-outer">
-        <div class="owl-stage" style="transform: translate3d(-4062px, 0px, 0px); transition: all 0.25s ease 0s; width: 9478px;">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <?php
+        $folder_path = '../../public/img/nosotros/nosotros_carrusel/';
+        $num_files = glob($folder_path . "*.{JPG,jpeg,gif,png,bmp}", GLOB_BRACE);
+        $folder = opendir($folder_path);
+        $index = 0;
+        if ($num_files > 0) {
+          while (false !== ($file = readdir($folder))) {
+            $file_path = $folder_path . $file;
+            $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+            if ($extension == 'jpg' || $extension == 'png' || $extension == 'gif' || $extension == 'bmp') {
+        ?>
+              <div class="carousel-item  <?php if ($index == 0) echo ("active");  ?>">
+                <img class="d-block w-100" src="<?php echo $file_path; ?>" alt="First slide">
 
-          <?php
-          $folder_path = '../../public/img/nosotros/nosotros_carrusel/';
-          $num_files = glob($folder_path . "*.{JPG,jpeg,gif,png,bmp}", GLOB_BRACE);
-          $folder = opendir($folder_path);
-          $index=0;
-          if ($num_files > 0) {
-            while (false !== ($file = readdir($folder))) {
-              $file_path = $folder_path . $file;
-              $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
-              if ($extension == 'jpg' || $extension == 'png' || $extension == 'gif' || $extension == 'bmp') {
-          ?>
-
-                <div class="owl-item <?php if($index == 0) echo("active");  ?>" style="width: 1354px;">
-                  <div class="item">
-                    <div class="slider-caption h2slider">
-            
-                    </div>
-                    <img class="tfoto" src="<?php echo $file_path; ?>" alt="Imagen de ejemplo" title="Imagen de ejemplo">
-                      
-                  </div>
-                </div>
-          <?php
-          $index++;
-              }
+              </div>
+        <?php
+              $index++;
             }
           }
-          closedir($folder);
-          ?>
-        </div>
+        }
+        closedir($folder);
+        ?>
+
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-      <div class="owl-nav"><button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span><i class="fas fa-chevron-left"></i></button>
-      <button type="button" role="presentation" class="owl-next"><span aria-label="Next"></span><i class="fas fa-chevron-right"></i></button></div>
-      <div class="owl-dots disabled"></div>
-    </section>
 
 
 
 
 
-    <!-- FIN CARRUSEL -->
 
 
-    <!-- INICIO CUERPO INFORMACION NOSOTROS-->
+      <!-- FIN CARRUSEL -->
 
-    <div class="nosotros-cuerpo">
-      <?php
-      $controlador_nosotros = new NosotrosController();
-      echo ($controlador_nosotros->getNosotros());
-      ?>
+
+      <!-- INICIO CUERPO INFORMACION NOSOTROS-->
+
+      <div class="nosotros-cuerpo">
+        <?php
+        $controlador_nosotros = new NosotrosController();
+        echo ($controlador_nosotros->getNosotros());
+        ?>
+      </div>
+
+      <!-- INICIO CUERPO INFORMACION NOSOTROS-->
+
     </div>
-
-    <!-- INICIO CUERPO INFORMACION NOSOTROS-->
-
-  </div>
-  <?php include(TEMPLATES_PATH . "/footer.php") 
-  ?>
+    <?php include(TEMPLATES_PATH . "/footer.php")
+    ?>
 </body>
 
 </html>
